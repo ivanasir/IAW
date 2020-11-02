4c89f69e978d26a2a1b3665d88e02b1843e31196
ivanasir <ivan941824@gmail.com>
2020-11-02 16:57:51 +0100

    practicas1

---
+++ tests/ivan/presentacion.php
@@ -0,0 +1,35 @@
+<html>
+    <head>Presentacion</head>
+    <body>
+        <?php
+            $nombre = "ivan"
+            $apellidos = "garcia gonzalez "
+            $edad = "25"
+            $foto = ""
+            $cursos = array("ESO", " Instalaciones Eléctricas y Automáticas", "Sistemas Electrotécnicos y Automatizados")
+
+            echo "mi nombre es: ";
+            echo $nombre
+            echo "<br>";
+            echo "mis apellidos son: ";
+            echo $apellidos
+            echo "<br>";
+            echo "mi edad es: ";
+            echo $edad
+            echo "<br>";
+            echo "mis anteriores cursos son ";
+            echo . $cursos[0] . ", " . $cursos[1] . ", " . $cursos[2] . ".";
+            echo "<br>";
+        ?>
+
+        <form action="presentacion.php" method="post">
+            Ingrese nombre:<input type="text" name="nombre"><br>
+            Ingrese su correo:<br>
+            <textarea name="correo"></textarea>
+            <br>
+            <input type="submit" value="enviar">
+        </form>
+
+    </body>
+
+</html> 
