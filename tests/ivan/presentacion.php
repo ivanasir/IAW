f4f1d360f61251bfc922eb5a48d80d837a88d5d5
ivanasir <ivan941824@gmail.com>
2020-11-03 17:05:14 +0100

    presentacion

--- tests/ivan/presentacion.php
+++ tests/ivan/presentacion.php
@@ -7,6 +7,7 @@
             $edad = "25" ;
             $foto = "" ;
             $cursos = array("ESO", " Instalaciones Eléctricas y Automáticas", "Sistemas Electrotécnicos y Automatizados");
+            $foto = "<img src=https://cdn.dopl3r.com//media/memes_files/bob-esponja-pensando-plantilla-APImB.jpg";
 
             echo "mi nombre es: ";
             echo $nombre ;
@@ -20,6 +21,7 @@
             echo "mis anteriores cursos son ";
             echo $cursos[0] . ", " . $cursos[1] . ", " . $cursos[2] . ".";
             echo "<br>";
+            echo "$foto <br>";
         ?>
 
         <form action="presentacion.php" method="post">
