e28ee526288db752a7de210413b6801db527e712
ivanasir <ivan941824@gmail.com>
2020-11-04 17:34:38 +0100

    funciones

--- tests/ivan/practicasphp/funciones.php
+++ tests/ivan/practicasphp/funciones.php
@@ -1,9 +1,40 @@
 <?php 
     function openhtml(){
-      echo "<html>";
+      echo "<p>"."html"."</p>";
     }
 
     function closehtml(){
-       echo "</html>";
+       echo "<p>"."/html"."</p>";
     }
+
+    function openhead(){
+        echo "<"."head".">";
+    }
+
+    function closehead(){
+      echo "</head>";
+    }
+  
+    function opentitle(){
+    echo "<p>"."title"."</p>";
+    }
+    function closetitle(){
+     echo "<p>"."/title"."</p>";
+    }
+    function openbody(){
+      echo "<p>"."body"."</p>";
+    }
+    function closebody(){
+      echo "<p>"."/body"."</p>";
+    }
+    function say(){
+      echo "hola";
+    }
+    openhtml();
+    openhead();
+    opentitle();
+    say();
+    closetitle();
+    closehead();
+    closehtml();
 ?>
---
+++ tests/ivan/practicasphp/funciones1.php
@@ -0,0 +1,26 @@
+
+<html>
+    <head>
+        <title>Este es el titulo</title>
+
+    </head>
+    <body>
+        <?php
+            function crearhtml(){
+              echo "<html>";
+              echo "<head>";
+              echo "<title>Este es el título</title>";
+              echo "</head>";
+              echo "<body>";
+              echo "<h1 class='red'>Este es el título</h1>";
+              echo "<p class='red'>Hola este es el contenido</p>";
+              echo "</body>";
+              echo "</html>";
+            }
+
+            crearhtml();
+        ?>
+
+    </body>
+</html>
+
