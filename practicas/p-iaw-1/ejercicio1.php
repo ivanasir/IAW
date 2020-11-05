d5364f257632c492ff951e1d7bcdfcfdf0041235
ivanasir <ivan941824@gmail.com>
2020-11-05 21:39:24 +0100

    ejercicios 1 y 2

---
+++ practicas/p-iaw-1/ejercicio1.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <?php
+            // ahora incrustamos php en el cuerpo del html
+
+            echo "este es el mensaje que quiero mostrar"."</br>";
+            echo "este tambien es el mensaje que quiero mostrar"."</br>";
+            
+        ?>
+    
+    </body>
+</html>
---
+++ practicas/p-iaw-1/ejercicio2.php
@@ -0,0 +1,5 @@
+<?php
+    for ($n =2; $n <= 20; $n+=2) {
+    echo "el resultado es: $n <br>";
+    }
+?> 
