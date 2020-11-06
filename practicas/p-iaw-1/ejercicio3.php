6d48c38d546e3de611181978ae505da591ddd3c1
ivanasir <ivan941824@gmail.com>
2020-11-06 19:51:07 +0100

    ejercicio3

--- practicas/p-iaw-1/ejercicio3.php
+++ practicas/p-iaw-1/ejercicio3.php
@@ -5,9 +5,6 @@
             function hora(){
                 echo "Hoy es : " . date("D d - m M - Y");
             }
-            function ta(){
-             
-            }
 
             hora() 
             
